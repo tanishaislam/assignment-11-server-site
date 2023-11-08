2b9ab6bbe3ebe335a804b7aec1b9531f0fc92325
tanishaislam <dev.eva.2006@gmail.com>
2023-11-08 12:31:45 +0600

    jwt token is created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -7,7 +8,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 
@@ -28,6 +35,24 @@ async function run() {
     await client.connect();
     const featureCollection = client.db('studyFeature').collection('studyCard');
     const assignmentCollection = client.db('studyFeature').collection('assignments');
+
+    //server related api
+
+    app.post('/jwt', async (req, res) =>{
+      const user = req.body;
+      console.log('user for token', user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      
+      res.
+      cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      .send({success: true})
+    })
+
+
 
 
     app.get('/studyCard', async(req, res)=>{
